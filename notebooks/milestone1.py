b6722e88f99bee32291a3db31eaadaa3d2f20c3e
michell <patelmish712@gmail.com>
2026-05-03 18:01:30 +0300

    milestone1.py

--- notebooks/milestone1.py
+++ notebooks/milestone1.py
@@ -1,34 +1,24 @@
-# %% [1] SETUP & DATA LOADING
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-# Load the dataset
 file_path = "C:\\Users\\ADMIN\\OneDrive\\Desktop(1)\\ecommerce-customer-analytics\\E-commerce Customer Behavior - Sheet1.csv"
 df = pd.read_csv(file_path)
 
-# %% [2] DATA SCHEMA (Metadata)
-# This displays the column names, data types, and non-null counts
 print("--- DATASET SCHEMA ---")
 print(df.info())
 
-# %% [3] DESCRIPTIVE STATISTICS
-print("\n--- NUMERICAL SUMMARY ---")
-# .T (transpose) makes the statistics easier to read as a list
+print("NUMERICAL SUMMARY")
 print(df.describe().T)
 
-print("\n--- CATEGORICAL SUMMARY ---")
-# This summarizes non-numeric columns like Gender, City, and Satisfaction Level
+print(" CATEGORICAL SUMMARY")
 print(df.describe(include=['object']).T)
 
-print("\n--- SATISFACTION VALUE COUNTS ---")
+print("SATISFACTION VALUE COUNTS ")
 print(df['Satisfaction Level'].value_counts())
 
-# %% [4] DATA VISUALIZATION
-# Set global style
 sns.set_theme(style="whitegrid")
 
-# Create a 2x2 grid for the plots
 fig, axes = plt.subplots(2, 2, figsize=(15, 12))
 
 # Visual 1: Distribution of Total Spend (Histplot)
@@ -52,6 +42,5 @@ axes[1, 0].set_ylabel('Number of Customers')
 sns.scatterplot(x='Items Purchased', y='Average Rating', hue='Gender', data=df, alpha=0.7, ax=axes[1, 1])
 axes[1, 1].set_title('4. Items Purchased vs. Average Rating', fontsize=14)
 
-# Final formatting adjustments
 plt.tight_layout()
 plt.show()
